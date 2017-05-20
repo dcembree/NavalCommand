6de3c6aa3df3496d2ca0c68487019c690b8cc90d
David Embree <dcembree@gmail.com>
2017-05-20 01:22:37 -0400

    Added property tags that are utilized by the Razor renderer to set data labels.

--- NavalCommand/Models/Vessel.cs
+++ NavalCommand/Models/Vessel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Web;
@@ -15,38 +16,55 @@ namespace NavalCommand.Models
 
         #region Properties
 
+        [DisplayName("Serial Number")]
         public int Id { get; set; }
 
+        [DisplayName("Vessel Name")]
         public string Name { get; set; }
 
+        [DisplayName("Vessel Class")]
         public string VesselClass { get; set; }
 
+        [DisplayName("Home Port")]
         public string HomePort { get; set; }
 
+        [DisplayName("Commission Date")]
         public DateTime CommissionDate { get; set; }
 
+        [DisplayName("Decommision Date")]
         public DateTime? DecommissionDate { get; set; }
 
+        [DisplayName("Hull Configuration")]
         public string HullConfiguration { get; set; }
 
+        [DisplayName("Hull Status")]
         public string HullStatus { get; set; }
 
+        [DisplayName("Damage Report")]
         public string[] DamageReport { get; set; }
 
+        [DisplayName("Deployment Status")]
         public string DeploymentStatus { get; set; }
 
+        [DisplayName("Crew Complement")]
         public int CrewComplement { get; set; }
 
+        [DisplayName("Current Longitude")]
         public double CurrentLongitude { get; set; }
 
+        [DisplayName("Current Latitude")]
         public double CurrentLatitude { get; set; }
 
+        [DisplayName("Current Heading")]
         public double CurrentHeading { get; set; }
 
+        [DisplayName("Current Speed")]
         public double CurrentSpeed { get; set; }
 
+        [DisplayName("Current Depth")]
         public double CurrentDepth { get; set; }
 
+        [DisplayName("Current Orders")]
         public string CurrentOrders { get; set; }
 
         #endregion
